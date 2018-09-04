ef17f5a97a0d2ccbba50c2a3e587419be71a6f73
Rasmus Hansen <rasmusvhansen@gmail.com>
2018-09-04 21:41:54 +0200

    Added minivikar

--- index.js
+++ index.js
@@ -1,40 +1,41 @@
 require('dotenv').config();
-const difference = require("lodash/difference");
-const getOdooMembers = require("./odoo");
-const { addMembers, getMembers, deleteMembers } = require("./directory");
+const difference = require('lodash/difference');
+const getOdooMembers = require('./odoo');
+const { addMembers, getMembers, deleteMembers } = require('./directory');
 
 const groupMap = [
-  [3654, "sync-mini"],
-  [3652, "sync-fam"],
-  [8808, "sync-aser"],
-  [3657, "sync-heidrun"],
-  [3658, "sync-quark"],
-  [3656, "sync-trop"],
-  [3655, "sync-junior"],
+  [3654, 'sync-mini'],
+  [3652, 'sync-fam'],
+  [8808, 'sync-aser'],
+  [3657, 'sync-heidrun'],
+  [3658, 'sync-quark'],
+  [3656, 'sync-trop'],
+  [3655, 'sync-junior'],
+  [9073, 'sync-minivikar']
 ];
 
 async function main() {
   for (const [odooId, groupId] of groupMap) {
     // cannot run in parallel, since the api does not support concurrent usage...
-    await syncGroup(odooId, groupId + "@valhallagruppe.dk");
+    await syncGroup(odooId, groupId + '@valhallagruppe.dk');
   }
 }
 
 main()
-  .then(() => console.log("Synchronized successfully"))
-  .catch(console.error)
+  .then(() => console.log('Synchronized successfully'))
+  .catch(console.error);
 
 function syncGroup(odooId, groupId) {
-  const googleMembersPromise = getMembers(groupId).then(r => r.members ? r.members.map(u => u.email.toLowerCase()) : []);
+  const googleMembersPromise = getMembers(groupId).then(
+    r => (r.members ? r.members.map(u => u.email.toLowerCase()) : [])
+  );
   const odooMembersPromise = getOdooMembers(odooId, 1000).then(users => users.map(u => u.email.toLowerCase()));
-  return Promise.all([
-    googleMembersPromise,
-    odooMembersPromise]
-  ).then(([googleMembers, odooMembers]) => {
-    const toCreate = difference(odooMembers, googleMembers);
-    const toDelete = difference(googleMembers, odooMembers);
-    const toCreatePromise = toCreate.length ? addMembers(groupId, toCreate) : Promise.resolve();
-    return toCreatePromise.then(() => toDelete.length ? deleteMembers(groupId, toDelete) : Promise.resolve);
-  })
+  return Promise.all([googleMembersPromise, odooMembersPromise])
+    .then(([googleMembers, odooMembers]) => {
+      const toCreate = difference(odooMembers, googleMembers);
+      const toDelete = difference(googleMembers, odooMembers);
+      const toCreatePromise = toCreate.length ? addMembers(groupId, toCreate) : Promise.resolve();
+      return toCreatePromise.then(() => (toDelete.length ? deleteMembers(groupId, toDelete) : Promise.resolve));
+    })
     .catch(r => console.log(r));
 }
