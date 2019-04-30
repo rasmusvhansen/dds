4d8d0dcb78f7a40d67a74147037f4a0bb496582e
Rasmus Hansen <rasmusvhansen@gmail.com>
2019-04-30 22:14:51 +0200

    Enabled sync-all

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 require('dotenv').config();
 const difference = require('lodash/difference');
-const getOdooMembers = require('./odoo');
+const uniq = require('lodash/uniq');
+const getOdooMembersFromGroups = require('./odoo');
 const { addMembers, getMembers, deleteMembers } = require('./directory');
 const groupMap = JSON.parse(process.env.GROUPMAP);
 
 async function main() {
-  for (const [odooId, groupId] of groupMap) {
+  for (const [odooIds, groupId] of groupMap) {
     // cannot run in parallel, since the api does not support concurrent usage...
-    await syncGroup(odooId, groupId + '@valhallagruppe.dk');
+    await syncGroup(odooIds, groupId + '@valhallagruppe.dk');
   }
 }
 
@@ -15,15 +16,17 @@ main()
   .then(() => console.log('Synchronized successfully'))
   .catch(console.error);
 
-function syncGroup(odooId, groupId) {
-  const googleMembersPromise = getMembers(groupId).then(
-    r => (r.members ? r.members.map(u => u.email.toLowerCase()) : [])
+function syncGroup(odooIds, groupId) {
+  const googleMembersPromise = getMembers(groupId).then(r =>
+    r.members ? r.members.map(u => u.email.toLowerCase()) : []
   );
-  const odooMembersPromise = getOdooMembers(odooId, 1000).then(users => users.map(u => u.email.toLowerCase()));
+  const odooMembersPromise = getOdooMembersFromGroups(odooIds, 1000).then(users =>
+    users.map(u => u.email.toLowerCase())
+  );
   return Promise.all([googleMembersPromise, odooMembersPromise])
     .then(([googleMembers, odooMembers]) => {
-      const toCreate = difference(odooMembers, googleMembers);
-      const toDelete = difference(googleMembers, odooMembers);
+      const toCreate = uniq(difference(odooMembers, googleMembers));
+      const toDelete = uniq(difference(googleMembers, odooMembers));
       const toCreatePromise = toCreate.length ? addMembers(groupId, toCreate) : Promise.resolve();
       return toCreatePromise.then(() => (toDelete.length ? deleteMembers(groupId, toDelete) : Promise.resolve));
     })
--- odoo.js
+++ odoo.js
@@ -1,5 +1,5 @@
-const Odoo = require("odoo-xmlrpc");
-const uniqBy = require("lodash/uniqBy");
+const Odoo = require('odoo-xmlrpc');
+const uniqBy = require('lodash/uniqBy');
 const odoo = new Odoo({
   url: process.env.ODOOURL,
   port: 443,
@@ -9,84 +9,66 @@ const odoo = new Odoo({
 });
 
 const flatMap = (f, arr) => arr.reduce((x, y) => [...x, ...f(y)], []);
+async function getMembersForManyGroups(ids) {
+  if (ids instanceof Array) {
+    let allMembers = [];
+    for (const id of ids) {
+      // cannot run in parallel, since the api does not support concurrent usage...
+      const members = await getOdooMembers(id);
+      allMembers = [...allMembers, ...members];
+    }
+    return allMembers;
+  }
+  return getOdooMembers(ids);
+}
 
 function getOdooMembers(id, limit = 1000) {
   return new Promise((resolve, reject) => {
-    odoo.connect(function (err) {
+    odoo.connect(function(err) {
       if (err) {
         reject(err);
         return;
       }
-      console.log("Connected to Odoo server.");
+      console.log('Connected to Odoo server.');
       const params = [
-        [
-          ["member_id.function_ids.organization_id", "child_of", id],
-          ["organization_id", "=", 859]
-        ],
-        [
-          "organization_id",
-          "member_number",
-          "name",
-          "member_id",
-          "email",
-          "relation_all_ids"
-        ],
+        [['member_id.function_ids.organization_id', 'child_of', id], ['organization_id', '=', 859]],
+        ['organization_id', 'member_number', 'name', 'member_id', 'email', 'relation_all_ids'],
         0,
         limit
       ];
 
-      odoo.execute_kw("member.profile", "search_read", [params], function (
-        err,
-        value
-      ) {
+      odoo.execute_kw('member.profile', 'search_read', [params], function(err, value) {
         if (err) {
           reject(err);
-          return
+          return;
         }
         value = value || [];
-        const emails = value
-          .filter(m => m.email)
-          .map(m => ({ name: m.name, email: m.email }));
+        const emails = value.filter(m => m.email).map(m => ({ name: m.name, email: m.email }));
 
         const relationIds = flatMap(m => m.relation_all_ids, value);
 
-        const relationParams = [[relationIds, ["other_partner_id", "type_id"]]];
-        odoo.execute_kw(
-          "res.partner.relation.all",
-          "read",
-          relationParams,
-          function (err, value) {
+        const relationParams = [[relationIds, ['other_partner_id', 'type_id']]];
+        odoo.execute_kw('res.partner.relation.all', 'read', relationParams, function(err, value) {
+          if (err) {
+            reject(err);
+            return;
+          }
+          value = value || [];
+          const idsToFetch = value.filter(m => m.type_id[1] === 'Forældre til').map(m => m.other_partner_id[0]);
+
+          const parentParams = [[idsToFetch, ['email', 'name']]];
+          odoo.execute_kw('res.partner', 'read', parentParams, function(err, value) {
             if (err) {
               reject(err);
-              return;
             }
             value = value || [];
-            const idsToFetch = value
-              .filter(m => m.type_id[1] === "Forældre til")
-              .map(m => m.other_partner_id[0]);
-
-            const parentParams = [[idsToFetch, ["email", "name"]]];
-            odoo.execute_kw("res.partner", "read", parentParams, function (
-              err,
-              value
-            ) {
-              if (err) {
-                reject(err);
-              }
-              value = value || [];
-              const allEmails = [
-                ...emails,
-                ...value
-                  .filter(m => m.email)
-                  .map(m => ({ name: m.name, email: m.email }))
-              ];
-              resolve(uniqBy(allEmails, p => p.email));
-            });
-          }
-        );
+            const allEmails = [...emails, ...value.filter(m => m.email).map(m => ({ name: m.name, email: m.email }))];
+            resolve(uniqBy(allEmails, p => p.email));
+          });
+        });
       });
     });
   });
 }
 
-module.exports = getOdooMembers;
+module.exports = getMembersForManyGroups;
